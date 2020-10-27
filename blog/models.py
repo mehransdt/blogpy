a9330538c127ca6daf4c2afd3f4f53ab20a72e5a
Mehran <mehran@saadat.org>
2020-10-27 17:22:35 +0330

    create models and add ckeditor module

--- blog/models.py
+++ blog/models.py
@@ -3,17 +3,41 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 from datetime import datetime
 
+def validate_file_extension(value):
+    import os
+    from django.core.exceptions import ValidationError
+    ext = os.path.splitext(value.name)[1]
+    valid_extension = ['.jpg','.png']
+    if not ext.lower() in valid_extension:
+        raise ValidationError('file extension not supported')
+
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE)
-    avatar = models.FileField(upload_to='files/user_avatar',null=True , blank=True)
+    avatar = models.FileField(upload_to='files/user_avatar',null=True , blank=True , validators=[validate_file_extension])
     description = models.CharField(max_length=512,null=False,blank=False)
+
+    def __str__(self):
+        return self.user.first_name + ' ' + self.user.last_name
 
 class Article(models.Model):
     title = models.CharField(max_length=128,null=False,blank=False)
-    cover = models.FileField(upload_to='files/article_cover',null=False,blank=False)
+    cover = models.FileField(upload_to='files/article_cover',null=False,blank=False , validators=[validate_file_extension])
     content = RichTextField()
     created_at = models.DateTimeField(default=datetime.now)
-    
+    categoty = models.ForeignKey('category',on_delete=models.CASCADE)
+    author = models.OneToOneField(UserProfile,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Category(models.Model):
+    title = models.CharField(max_length=128 , blank=False ,null=False)
+    cover = models.FileField(upload_to='files/category_cover',null=False , blank=False , validators=[validate_file_extension])
+
+    def __str__(self):
+        return self.title
 
 
 
