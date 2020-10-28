0a94cf15ce2b862b6f070457c63819442dc16c3b
Mehransdt <almaitco@gmail.com>
2020-10-28 11:02:47 +0330

    admin models and requirments file

--- blog/admin.py
+++ blog/admin.py
@@ -2,6 +2,19 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(UserProfile)
-admin.site.register(Category)
-admin.site.register(Article)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['user','avatar','description']
+
+admin.site.register(UserProfile,UserProfileAdmin)
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ['title','content']
+    list_display = ['title','content','created_at']
+
+admin.site.register(Article,ArticleAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title','cover']
+
+admin.site.register(Category,CategoryAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -18,8 +18,6 @@ class UserProfile(models.Model):
     avatar = models.FileField(upload_to='files/user_avatar',null=True , blank=True , validators=[validate_file_extension])
     description = models.CharField(max_length=512,null=False,blank=False)
 
-    def __str__(self):
-        return self.user.first_name + ' ' + self.user.last_name
 
 class Article(models.Model):
     title = models.CharField(max_length=128,null=False,blank=False)
@@ -29,15 +27,9 @@ class Article(models.Model):
     categoty = models.ForeignKey('category',on_delete=models.CASCADE)
     author = models.OneToOneField(UserProfile,on_delete=models.CASCADE)
 
-    def __str__(self):
-        return self.title
 
 class Category(models.Model):
     title = models.CharField(max_length=128 , blank=False ,null=False)
     cover = models.FileField(upload_to='files/category_cover',null=False , blank=False , validators=[validate_file_extension])
-
-    def __str__(self):
-        return self.title
-
 
 
